435d2d400daabfa5efe376c3ba7e63ce456b8476
Vaibhav <Vaibhavspacegupta@gmail.com>
2015-09-04 17:49:10 +0530

    adding tickets

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,7 @@
 class ProjectsController < ApplicationController
 
+before_action :set_project, only: [:show,:edit,:update,:destroy]
+
 def index
 	@projects = Project.all
 
@@ -24,17 +26,13 @@ def create
 end
 
 def show
- @project = Project.find(params[:id])
-
 end
 
 def edit
-@project = Project.find(params[:id])
 end
 
 
 def update
-	@project = Project.find(params[:id])
 	if @project.update(project_params)
 		flash[:notice] = "Project has been updated."
 		redirect_to @project
@@ -44,9 +42,26 @@ def update
 	end
 end
 
+
+def destroy
+
+	@project.destroy
+
+	flash[:notice] = "Project has been destroyed. "
+	redirect_to projects_path
+end
 private
 
 def project_params
       params.require(:project).permit(:name, :description)
   end
+
+def set_project
+	@project = Project.find(params[:id])
+	rescue ActiveRecord::RecordNotFound
+	flash[:alert] = "The project you were looking" +
+	" for could not be found."
+	redirect_to projects_path
+end
+
 end
