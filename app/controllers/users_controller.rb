17a23a8ab41e748fd39b47833fb050483fc6be0f
Vaibhav <Vaibhavspacegupta@gmail.com>
2015-09-06 11:43:00 +0530

    Implemented Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new 
   end
 
   def show
   end
 
   def create
+  	@user = User.new(user_params)
+
+  	if @user.save
+  		flash[:notice] = "You have signed up successfully."
+  		redirect_to projects_path
+  	else
+  		flash[:alert] = "Try Again"
+  		render :new
+  	end
+
   end
+
+private 
+	def user_params
+		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	end
 end
