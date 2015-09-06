2358f312c24f3f468085230f429c0e771b49125c
Vaibhav <Vaibhavspacegupta@gmail.com>
2015-09-06 11:19:42 +0530

    back

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
+  get 'users/show'
+
+  get 'users/create'
+
   root "projects#index"
 
   resources :projects do 
     resources :tickets
   end
 
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
